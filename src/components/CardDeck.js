26af502e94fc8bc4d2865bf0f1e88e81f88de3b7
Aazain <43759637+Aazainkhan@users.noreply.github.com>
2023-08-20 00:29:33 -0400

    card swipe feature

--- src/components/CardDeck.js
+++ src/components/CardDeck.js
@@ -14,14 +14,9 @@ const imageDeck = [
 const to = i => ({ x: 0, y: i * -4, scale: 1, rot: -10 + Math.random() * 20, delay: i * 100 })
 const from = i => ({ x: 0, rot: 0, scale: 1.5, y: -1000 })
 // This is being used down there in the view, it interpolates rotation and scale into a css transform
-const trans = (r, s) => `perspective(1500px) rotateX(30deg) rotateY(${r / 10}deg) rotateZ(${r}deg) scale(${s})`
+const trans = (r, s) => `perspective(1500px) rotateX(0) rotateY(${r / 10}deg) rotateZ(${r}deg) scale(${s})`
 
-/**
- * Randomize array element order in-place.
- * Using Durstenfeld shuffle algorithm.
- * Updated for EC6/ECMA2015
- * source: https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
- */
+
 function shuffleArray(array) {
     for (let i = array.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1))
@@ -35,8 +30,8 @@ function CardDeck() {
     const [gone] = useState(() => new Set()) // The set flags all the cards that are flicked out
     const [props, set] = useSprings(cards.length, i => ({ ...to(i), from: from(i) })) // Create a bunch of springs using the helpers above
     // Create a gesture, we're interested in down-state, delta (current-pos - click-pos), direction and velocity
-    const bind = useDrag(({ args: [index], down, delta: [xDelta], distance, direction: [xDir], velocity }) => {
-        const trigger = velocity > 0.2 // If you flick hard enough it should trigger the card to fly out
+    const bind = useDrag(({ args: [index], down, movement: [xDelta], distance, direction: [xDir], velocity }) => {
+        const trigger = velocity > 0.3 // If you flick hard enough it should trigger the card to fly out
         const dir = xDir < 0 ? -1 : 1 // Direction should either point left or right
         if (!down && trigger) gone.add(index) // If button/finger's up and trigger velocity is reached, we flag the card ready to fly out
         set(i => {
@@ -45,7 +40,12 @@ function CardDeck() {
             const x = isGone ? (200 + window.innerWidth) * dir : down ? xDelta : 0 // When a card is gone it flys out left or right, otherwise goes back to zero
             const rot = xDelta / 100 + (isGone ? dir * 10 * velocity : 0) // How much the card tilts, flicking it harder makes it rotate faster
             const scale = down ? 1.1 : 1 // Active cards lift up a bit
-            return { x, rot, scale, delay: undefined, config: { friction: 50, tension: down ? 800 : isGone ? 200 : 500 } }
+            return { x,
+                rot, 
+                scale, 
+                delay: undefined, 
+                config: { friction: 50, 
+                    tension: down ? 1000 : isGone ? 100 : 500 } }
         })
         if (!down && gone.size === cards.length) setTimeout(() => gone.clear() || set(i => to(i)), 600)
     })
@@ -55,15 +55,11 @@ function CardDeck() {
         <div className='card-deck'>
             {props.map(({ x, y, rot, scale }, i) => (
                 <animated.div key={i} className="card" style={{ transform: interpolate([x, y], (x, y) => `translate3d(${x}px,${y}px,0)`) }}>
-                    {
-                        // This is the card itself, we're binding our gesture to it (and inject its index so we know which is which)
-                    }
                     <animated.div {...bind(i)} style={{ transform: interpolate([rot, scale], trans), backgroundImage: `url(${cards[i]})` }} />
                 </animated.div>
             ))}
         </div>
     );
 }
-
 
 export default CardDeck;
