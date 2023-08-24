d19f0a7fd4fc2ffd80d9699654ef084435338544
Aazain <43759637+Aazainkhan@users.noreply.github.com>
2023-08-24 19:49:56 -0400

    component created

---
+++ src/components/WhatidoCards.js
@@ -0,0 +1,82 @@
+function WhatidoCards (){
+    return (
+        <>
+
+<section className="service">
+
+<h3 className="h3 service-title">What I do</h3>
+
+<ul className="service-list">
+
+    <li className="service-item">
+
+        <div className="service-icon-box">
+            <img src="./images/icon-dev.svg" alt="Web development icon" width="40" />
+        </div>
+
+        <div className="service-content-box">
+            <h4 className="h4 service-item-title">Web Development</h4>
+
+            <p className="service-item-text">
+                I design and build high-quality web applications which are responsive and user-friendly.
+            </p>
+        </div>
+
+    </li>
+
+    <li className="service-item">
+
+        <div className="service-icon-box">
+            <img src="./images/icon-prob.svg" alt="Problem solving icon" width="55" />
+        </div>
+
+        <div className="service-content-box">
+            <h4 className="h4 service-item-title">Problem-Solving</h4>
+
+            <p className="service-item-text">
+                Able to dissect broad and/or complex goals into achievable tasks.
+            </p>
+        </div>
+
+    </li>
+
+    <li className="service-item">
+
+        <div className="service-icon-box">
+            <img src="./images/icon-fix.svg" alt="Experimentation" width="50" />
+        </div>
+
+        <div className="service-content-box">
+            <h4 className="h4 service-item-title">Experiment</h4>
+
+            <p className="service-item-text">
+                Explore and pitch technological solutions to real-world problems.
+            </p>
+        </div>
+
+    </li>
+
+    <li className="service-item">
+
+        <div className="service-icon-box">
+            <img src="./images/icon-art.svg" alt="Digital Art Icon" width="50" />
+        </div>
+
+        <div className="service-content-box">
+            <h4 className="h4 service-item-title">Digital Art</h4>
+
+            <p className="service-item-text">
+                High quality digital art and graphic design where I experiment using my Adobe tools and canvases.
+            </p>
+        </div>
+
+    </li>
+
+</ul>
+
+</section>
+        </>
+    )
+}
+
+export default WhatidoCards;
