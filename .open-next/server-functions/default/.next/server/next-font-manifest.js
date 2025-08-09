05759c67fd2aaed109d872af7ea9d1df8a631ced
Aazain <aazainkhan@gmail.com>
2025-08-09 09:41:01 -0400

    Remove .open-next from tracking

--- .open-next/server-functions/default/.next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST='{"pages":{},"app":{"/Users/aazain/Documents/GitHub/aazainkhan.github.io/app/layout":["static/media/e4af272ccee01ff0-s.p.woff2"],"/Users/aazain/Documents/GitHub/aazainkhan.github.io/app/page":["static/media/e4af272ccee01ff0-s.p.woff2"]},"appUsingSizeAdjust":true,"pagesUsingSizeAdjust":false}';
